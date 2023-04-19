c45865f1f1fad3206256a416d1e0f85b923456e0
Mehtap Tataroğlu <teytap@gmail.com>
2023-04-19 08:27:51 +0300

    cleared unnecessary items added watchlist.js

--- App.js
+++ App.js
@@ -2,24 +2,79 @@ const moviesContainer = document.getElementById("movies-container");
 const newCard = document.getElementById("new-card");
 const movieInput = document.getElementById("movie-input");
 const searchBtn = document.getElementById("button-addon2");
-console.log(movieInput.value);
+const addBtn = document.getElementById("add-btn");
+
 let title = "";
+let searchedMoviesArray = [];
+
+document.addEventListener("click", (e) => {
+  //-------add button---------
+  if (e.target.id === `add-btn`) {
+    let localArray = [];
+    const addButtons = document.getElementsByClassName("film-btn");
+    //toggle buttons
+    const targetBtn = Object.values(addButtons).filter((btn) => {
+      return btn.dataset.imdb === e.target.dataset.imdb;
+    });
+    targetBtn[1].classList.toggle("hidden");
+    targetBtn[0].classList.toggle("hidden");
+    //add to local storage
+    const targetMovies = searchedMoviesArray.filter((movie) => {
+      return movie.imdbID === e.target.dataset.imdb;
+    });
+    if (localStorage.getItem("watchlistMovies")) {
+      localArray = JSON.parse(localStorage.getItem("watchlistMovies"));
+    }
+    if (localArray.indexOf(targetMovies[0]) == -1 || localArray.length === 0) {
+      localArray.push(targetMovies[0]);
+    }
+    localStorage.setItem("watchlistMovies", JSON.stringify(localArray));
+    console.log(targetMovies[0]);
+  }
+
+  //------remove button------
+  if (e.target.id === "remove-btn") {
+    const addButtons = document.getElementsByClassName("film-btn");
+    const targetBtn = Object.values(addButtons).filter((btn) => {
+      return btn.dataset.imdb === e.target.dataset.imdb;
+    });
+    const oldArray = JSON.parse(localStorage.getItem("watchlistMovies"));
+    targetBtn[0].classList.toggle("hidden");
+    targetBtn[1].classList.toggle("hidden");
+    const newArray = oldArray.filter((movie) => {
+      return movie.imdbID !== e.target.dataset.imdb;
+    });
+    localStorage.setItem("watchlistMovies", JSON.stringify(newArray));
+  }
+});
 
 movieInput.addEventListener("change", (e) => {
   e.preventDefault();
-  title = movieInput.value.toLowerCase().replace(" ", "+");
-  console.log(title);
+  title = movieInput.value.toLowerCase().split(" ").join("+");
 });
 searchBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  renderMovies();
+  try {
+    renderMovies();
+  } catch (e) {
+    moviesContainer.innerHTML = `<div class='no-results'><p>Unable to find what you’re looking for. Please try another search.</p></div>`;
+  }
 });
 
 function getMovieCardHtml(data) {
-  let movieCardHtml = "";
+  let poster = "";
 
-  movieCardHtml = ` <img src="${data.Poster}" alt="movie-img" class="poster" />
-            <div id="movie-info">
+  if (data.Poster === "N/A" || data.Poster === undefined) {
+    poster = "/images/movie.png";
+  } else {
+    poster = data.Poster;
+  }
+
+  moviesContainer.innerHTML += ` 
+  <div id="new-card" class="movie-card"><img src="${poster}" alt="${
+    data.Title
+  }-img" class="poster" />
+        <div id="movie-info">
               <div id="movie-title">
                 <span class="movie-title">${data.Title}</span>
                 <span class="movie-rate"> ⭐️ ${data.Ratings[0].Value.slice(
@@ -27,26 +82,48 @@ function getMovieCardHtml(data) {
                   3
                 )}</span>
               </div>
-              <div>
+            <div>
                 <span class="movie-runtime">${data.Runtime}</span
                 ><span class="movie-genre">${data.Genre}</span
-                >
-                <button id="add-btn">
+                ><button class="film-btn hidden" id="remove-btn" data-imdb = ${
+                  data.imdbID
+                }>
+                  <img src="images/minus-icon.png" alt="" />
+                  Remove
+                </button>
+                <button class="film-btn" id="add-btn" data-imdb = ${
+                  data.imdbID
+                }>
                   <img src="images/plus-icon.png" alt="" />
                   Watchlist
-                </button></div>
+                </button>
+                
+            </div>
               <p>
                 ${data.Plot}
-              </p>`;
-  newCard.innerHTML = movieCardHtml;
+              </p>
+        </div>
+    </div>`;
 }
+
 function renderMovies() {
-  //fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=124ce01e`)
-  fetch(`http://www.omdbapi.com/?t=${title}&apikey=124ce01e`)
+  fetch(`http://www.omdbapi.com/?s=${title}&apikey=124ce01e`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      moviesContainer.innerHTML = "";
+      if (data.Search) {
+        for (data of data.Search) {
+          let searchTitle = data.Title.toLowerCase().split(" ").join("+");
 
-      getMovieCardHtml(data);
+          fetch(`http://www.omdbapi.com/?t=${searchTitle}&apikey=124ce01e`)
+            .then((res) => res.json())
+            .then((data) => {
+              searchedMoviesArray.push(data);
+              getMovieCardHtml(data);
+            });
+        }
+      } else {
+        moviesContainer.innerHTML = `<p class="not-found">Unable to find what you’re looking for. Please try another search.</p>`;
+      }
     });
 }
---
+++ watchlist.js
@@ -0,0 +1,67 @@
+const watchlistContainer = document.getElementById("watchlist-container");
+//----remove button------
+document.addEventListener("click", (e) => {
+  if (e.target.id === "remove-btn") {
+    const oldArray = JSON.parse(localStorage.getItem("watchlistMovies"));
+
+    const newArray = oldArray.filter((movie) => {
+      return movie.imdbID != e.target.dataset.imdb;
+    });
+    console.log(newArray);
+    localStorage.setItem("watchlistMovies", JSON.stringify(newArray));
+
+    watchlistContainer.innerHTML = getWatchlistHtml();
+  }
+});
+//----shows movie list----
+function getWatchlistHtml() {
+  let watchlistHtml = "";
+
+  const watchlist = JSON.parse(localStorage.getItem("watchlistMovies"));
+  if (watchlist.length > 0) {
+    for (watch of watchlist) {
+      let poster = "";
+
+      if (watch.Poster === "N/A" || watch.Poster === undefined) {
+        poster = "/images/movie.png";
+      } else {
+        poster = watch.Poster;
+      }
+      watchlistHtml += ` 
+      <div id="new-card" class="movie-card">
+          <img src="${poster}" alt="${watch.Title}-img" class="poster" />
+          <div id="movie-info">
+                <div id="movie-title">
+                  <span class="movie-title">${watch.Title}</span>
+                  <span class="movie-rate"> ⭐️ ${watch.Ratings[0].Value.slice(
+                    0,
+                    3
+                  )}</span>
+            </div>
+            <div>
+                  <span class="movie-runtime">${watch.Runtime}</span
+                  ><span class="movie-genre">${watch.Genre}</span
+                  ><button class="film-btn" id="remove-btn" data-imdb = ${
+                    watch.imdbID
+                  }>
+                    <img src="images/minus-icon.png" alt="" />
+                    Remove
+                  </button>
+            </div>
+                <p>${watch.Plot}</p>
+            </div>
+      </div>`;
+    }
+  } else {
+    watchlistHtml = `
+                <div class="no-movie-container">
+          <h4>Your watchlist is looking a little empty...</h4>
+          <a href="/index.html">
+            <img src="images/plus-icon.png" alt="" /> Let's add some movies!
+          </a>
+        </div>
+            `;
+  }
+  return watchlistHtml;
+}
+watchlistContainer.innerHTML = getWatchlistHtml();
